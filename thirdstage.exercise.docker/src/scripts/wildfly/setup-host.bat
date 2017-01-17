f5e8bd506debed627452d8472a2af20c8d95be66
3rdstage <halfface@chollian.net>
2017-01-17 04:54:32 +0000

    ...

--- thirdstage.exercise.docker/src/scripts/wildfly/setup-host.bat
+++ thirdstage.exercise.docker/src/scripts/wildfly/setup-host.bat
@@ -2,6 +2,24 @@
 @ echo off
 setLocal enableDelayedExpansion
 
+
+if not exist "%SystemRoot%\System32\drivers\etc\hosts" (
+   echo.
+   echo Can't find 'hosts' file under '%SystemRoot%\System32\drivers'
+   echo Check your computer has installed Microsoft Windows properly.
+   goto :END
+)
+
+for /f "tokens=*" %%l in (%SystemRoot%\System32\drivers\etc\hosts) do (
+   echo %%l
+
+   for /f "tokens=1,2* delims=' '" %%a in ("%%l") do (
+      echo %%a ","
+   )
+)
+
+goto :END
+
 if not exist "%VBOX_MSI_INSTALL_PATH%" (
    if not exist "%VBOX_INSTALL_PATH%" (
       echo.
@@ -16,7 +34,7 @@ mkdir c:\var
 "%VBOX_MSI_INSTALL_PATH%\VBoxManage" sharedfolder add default --name c/var --hostpath c:\var --automount
 
 docker-machine ssh default sudo mkdir /c/var
-   
+
 docker-machine ssh defalut sudo mount -t vboxsf -o uid=1000,gid=50 c/var /c/var
 
 :END
