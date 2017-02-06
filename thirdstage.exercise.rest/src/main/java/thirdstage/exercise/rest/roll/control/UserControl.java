a45797f9ca11483b8afa0bfea93dacbee50e405a
Sangmoon Oh <Sangmoon Oh@SKCC16N01129.SKCC.NET>
2017-02-06 10:35:36 +0000

    ...

--- thirdstage.exercise.rest/src/main/java/thirdstage/exercise/rest/roll/control/UserControl.java
+++ thirdstage.exercise.rest/src/main/java/thirdstage/exercise/rest/roll/control/UserControl.java
@@ -1,11 +1,22 @@
 package thirdstage.exercise.rest.roll.control;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import thirdstage.exercise.rest.roll.entity.User;
 
 @Api("User")
 @RestController
-public class UserControl {
+public class UserControl{
+
+   @ApiOperation("Find a user of the specified ID")
+   @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE}, value = "users/{userId}")
+   public User getUserById(@PathVariable("userId") String id){
+      return new User();
+   }
 
 }
