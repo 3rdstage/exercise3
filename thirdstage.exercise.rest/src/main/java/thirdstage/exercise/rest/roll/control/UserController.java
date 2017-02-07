3f0c1180bc1e0dd66811d7e51662f57b1073112d
3rdstage <halfface@chollian.net>
2017-02-08 00:05:55 +0900

    ...

--- thirdstage.exercise.rest/src/main/java/thirdstage/exercise/rest/roll/control/UserController.java
+++ thirdstage.exercise.rest/src/main/java/thirdstage/exercise/rest/roll/control/UserController.java
@@ -1,9 +1,9 @@
 package thirdstage.exercise.rest.roll.control;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,10 +11,11 @@ import thirdstage.exercise.rest.roll.entity.User;
 
 @Api("User")
 @RestController
-public class UserControl{
+@RequestMapping("/users")
+public class UserController{
 
    @ApiOperation("Find a user of the specified ID")
-   @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE}, value = "users/{userId}")
+   @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE}, value = "/{userId}")
    public User getUserById(@PathVariable("userId") String id){
       return new User();
    }
