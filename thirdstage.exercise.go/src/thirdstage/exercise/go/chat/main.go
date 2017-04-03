d16666a00e9d1e92087d2a7a2e2a37fd6a793946
3rdstage <halfface@chollian.net>
2017-04-04 00:32:00 +0900

    ...

--- thirdstage.exercise.go/src/thirdstage/exercise/go/chat/main.go
+++ thirdstage.exercise.go/src/thirdstage/exercise/go/chat/main.go
@@ -3,18 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-          <html>
-            <head><title>Chat</title></head>
-            <body>
-            Let's chat
-            </body>
-          </html>`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+
+	t.templ.Execute(w, r)
+
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
