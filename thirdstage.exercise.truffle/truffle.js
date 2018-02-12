2d7d95d6cadff1aba28872cf2e8cc5bb58565eec
3rdstage <halfface@chollian.net>
2018-02-13 02:17:20 +0900

    ...

--- thirdstage.exercise.truffle/truffle.js
+++ thirdstage.exercise.truffle/truffle.js
@@ -7,6 +7,14 @@ module.exports = {
         from: "d4535798e632789fbe5867e84ed4b9af1bbfed38",
         gasPrice: 0,
         gas: 0x2fefd8
+      },
+      ganache: {
+        host: "127.0.0.1",
+        port: 8555,
+        network_id: 37,
+        from: "90f8bf6a479f320ead074411a4b0e7944ea8c9c1",
+        gasPrice: 20000000000,
+        gas: 90000
       }
     },
 
