f1eecaad622fc35bf06b2b2d0075fb8a20aa81fa
3rdstage <halfface@chollian.net>
2018-03-29 19:29:09 +0900

    ...

---
+++ thirdstage.exercise.truffle.webpack/temp/test.js
@@ -0,0 +1,8 @@
+var firstName = "xx";
+var lastName  = "xy";
+var phone     = "xz";
+var adress    = "x1";
+var obj = {"firstName":firstName, "lastName":lastName, "phone":phone, "address":adress};
+console.log(obj);
+
+console.log({"firstName":firstName, "lastName":lastName, "phone":phone, "address":adress});
