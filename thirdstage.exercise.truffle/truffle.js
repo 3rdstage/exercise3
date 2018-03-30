7cfccf47e79f2b0f6e04640ae76221662bac609b
3rdstage <halfface@chollian.net>
2018-03-30 10:25:57 +0900

    ...

--- thirdstage.exercise.truffle/truffle.js
+++ thirdstage.exercise.truffle/truffle.js
@@ -4,24 +4,23 @@ require("babel-register")({
   "plugins": ["syntax-async-functions","transform-regenerator"]
 });
 
+const fs = require('fs');
+
+const accounts = fs.readFileSync('scripts/quorum/run/accounts').toString().split('\n');
+const config = fs.readFileSync('scripts/quorum/config.sh').toString();
+const rpcPort = config.match(/\[rpcport\]=[0-9]*/g)[0].substring(10);
+const networkId = config.match(/\[networkid\]=[0-9]*/g)[0].substring(12);
+
 module.exports = {
    // http://truffleframework.com/docs/advanced/configuration
     networks: {
       development: {
         host: "192.168.56.101",
-        port: 8545,
-        network_id: 1991,
-        from: "74b2df69b079ab425c3636a814e19104b7eb6d08",
+        port: rpcPort,
+        network_id: networkId,
+        from: accounts[0],
         gasPrice: 0,
         gas: 0x10000000
-      },
-      ganache: {
-        host: "127.0.0.1",
-        port: 8555,
-        network_id: 37,
-        from: "e38e22817778c0ea8e422eebe0ad924df50ad239",
-        gasPrice: 20000000000,
-        gas: 90000
       }
     },
 
