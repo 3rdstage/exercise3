e4050812e866513edd9995ebc68a224cf16a7f87
3rdstage <halfface@chollian.net>
2018-04-19 19:51:29 +0900

    ...

--- thirdstage.exercise.ansible/prepare-controller.sh
+++ thirdstage.exercise.ansible/prepare-controller.sh
@@ -6,7 +6,11 @@
 # sudo apt-add-repository ppa:ansible/ansible
 # sudo apt-get update
 # sudo apt-get install ansible
-
+sudo apt-get -y update
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible 
 
 # Install Ansible Container : http://docs.ansible.com/ansible-container/installation.html
-sudo pip install ansible-container[docker,k8s]
+# sudo pip install ansible-container[docker,k8s]
