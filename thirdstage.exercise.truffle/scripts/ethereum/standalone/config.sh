86eb06f3c6af8cd97e37bee59861c1e0f879a9c3
3rdstage <halfface@chollian.net>
2018-06-14 23:11:35 +0900

    ...

--- thirdstage.exercise.truffle/scripts/ethereum/standalone/config.sh
+++ thirdstage.exercise.truffle/scripts/ethereum/standalone/config.sh
@@ -3,6 +3,6 @@
 declare -r network_id=31
 declare -r port=30303
 declare -r rpc_port=8545
-declare -r data_dir="$(cd `dirname $0` && cd ../../../ && pwd)/data/ethereum/solo"
+declare -r data_dir="$(cd `dirname $0` && cd ../../../ && pwd)/data/ethereum/standalone"
 declare -r passwd=user1234
 declare -r coin_base="0x0000000000000000000000000000000000000000"
