da0aa21068ba06e0d1c7aa014bc19fe4fb14d68c
3rdstage <halfface@chollian.net>
2018-06-18 10:17:11 +0900

    ...

--- thirdstage.exercise.ansible/scripts/cert/generate-tls-artifacts.sh
+++ thirdstage.exercise.ansible/scripts/cert/generate-tls-artifacts.sh
@@ -4,8 +4,24 @@
 #   https://help.ubuntu.com/community/OpenSSL
 #   https://www.openssl.org/docs/man1.1.0/apps/openssl-req.html
 
+readonly init_dir=$(pwd)
 readonly script_dir=$(cd `dirname $0` && pwd)
 
 cd ${script_dir}
 
-# Generate key and CSR for 
+# TODO Check whether or not the previous files exist
+# TODO Move back to the initial directory
+# TODO Notify the full paths of created files
+
+# Generate key and CSR
+openssl req \
+  -config sample.cnf \
+  -newkey rsa \
+  -keyout tls-server.key -keyform PEM \
+  -days 7300 -sha512 \
+  -out tls-server.csr -outform PEM
+  
+# Display the contents of CSR
+
+
+  
