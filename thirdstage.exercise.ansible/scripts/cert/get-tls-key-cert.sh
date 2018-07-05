4b6f61abd4cf3c52db185f543a93ffcadd33afa5
3rdstage <halfface@chollian.net>
2018-07-05 20:16:35 +0900

    ...

--- thirdstage.exercise.ansible/scripts/cert/get-tls-key-cert.sh
+++ thirdstage.exercise.ansible/scripts/cert/get-tls-key-cert.sh
@@ -8,18 +8,22 @@
 
 readonly script_dir=$(cd `dirname $0` && pwd)
 readonly url_base='https://github.com/3rdstage/exercise3/raw/master/thirdstage.exercise.ansible/scripts/cert'
-declare options=$*
+declare options=
 
-echo '$@ : '$@
-echo '$* : '$*
-echo $options
+for arg in $@; do
+  if [ ${arg:0:1} == '-' ]; then 
+    options=${options}" ${arg}"
+  else
+    options=${options}" '${arg}'"
+  if
+done
 
 curl -sSLOOO ${url_base}/{test-ca.key,test-ca.crt,test-tls.cnf}
  
 if [ -z "${options}" ]; then
   curl -sSL ${url_base}/generate-tls-artifacts.sh | bash -s
 else
-  curl -sSL ${url_base}/generate-tls-artifacts.sh | bash -s -- '${options}'
+  curl -sSL ${url_base}/generate-tls-artifacts.sh | bash -s -- ${options}
 fi
 
 # After generation, removes unnecessary files
