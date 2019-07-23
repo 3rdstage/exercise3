d832da69b677dffcc4dfcc7cf0b5482d3a0f55e5
Oh Sangmoon <halfface@chollian.net>
2019-07-23 20:47:54 +0900

    ...

--- thirdstage.exercise.springboot/src/test/scripts/redis/docker-redis-run.sh
+++ thirdstage.exercise.springboot/src/test/scripts/redis/docker-redis-run.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+# https://github.com/moby/moby/issues/24029
+export MSYS_NO_PATHCONV=1 
+
 readonly script_dir=$(cd `dirname $0` && pwd)
 
-
-docker run -v redis.conf:/usr/local/etc/redis/redis.conf --name redisserver redis redis-server /usr/local/etc/redis/redis.conf
+docker run -d --name local_redis \
+  -v ${script_dir}/redis.conf:/usr/local/etc/redis/redis.conf \
+  -p 6379:6379 \
+  redis redis-server /usr/local/etc/redis/redis.conf
