5e38c632d1abc8a128a0970a8598279df900962b
Sangmoon Oh <halfface@chollian.net>
2019-07-24 02:59:45 +0900

    ...

--- thirdstage.exercise.springboot/src/test/scripts/redis/docker-redis-run.sh
+++ thirdstage.exercise.springboot/src/test/scripts/redis/docker-redis-run.sh
@@ -4,8 +4,18 @@
 export MSYS_NO_PATHCONV=1 
 
 readonly script_dir=$(cd `dirname $0` && pwd)
+readonly container_name=local_redis
 
-docker run -d --name local_redis \
-  -v ${script_dir}/redis.conf:/usr/local/etc/redis/redis.conf \
+echo $script_dir
+cd $script_dir
+
+docker run -id --name ${container_name} \
   -p 6379:6379 \
-  redis redis-server /usr/local/etc/redis/redis.conf
+  redis bash
+  
+docker exec ${container_name} mkdir /usr/local/etc/redis
+docker cp redis.conf ${container_name}:/usr/local/etc/redis/redis.conf
+
+docker exec -d ${container_name} redis-server /usr/local/etc/redis/redis.conf
+
+# run 'docker exec local_redis redis-cli config get bind' to check
