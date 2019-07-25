be382214f197771fff05f1b57780895e44e9a15a
Oh Sangmoon <halfface@chollian.net>
2019-07-25 19:31:33 +0900

    ...

--- thirdstage.exercise.springboot/src/test/java/thirdstage/exercise/springboot/redis/RedisBasicTest.java
+++ thirdstage.exercise.springboot/src/test/java/thirdstage/exercise/springboot/redis/RedisBasicTest.java
@@ -7,9 +7,14 @@ import java.util.HashMap;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class RedisBasicTest{
 
 }
