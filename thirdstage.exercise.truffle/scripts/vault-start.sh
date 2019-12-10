5ff2e40808ed14e55c9bbacbbfaf3acdf039d802
Oh Sangmoon <halfface@chollian.net>
2019-12-10 16:52:58 +0900

    ...

--- thirdstage.exercise.truffle/scripts/vault-start.sh
+++ thirdstage.exercise.truffle/scripts/vault-start.sh
@@ -12,19 +12,23 @@ readonly vault_port="8200"
 readonly vault_log_level="debug"
 
 cd ${run_dir}
+readonly run_dir_win=$(pwd -W)
+mkdir -p storage
 
-cat <<EOF > vault-config.hcl
-// For Vault configuration, refer 'https://www.vaultproject.io/docs/configuration/'
-// For HCL, refer 'https://github.com/hashicorp/hcl/blob/hcl2/hclsyntax/spec.md'
-storage "file" {
-  path = "${run_dir}/storage"
-}
+if [ ! -f vault-config.hcl ]; then
+  cat <<EOF > vault-config.hcl
+  // For Vault configuration, refer 'https://www.vaultproject.io/docs/configuration/'
+  // For HCL, refer 'https://github.com/hashicorp/hcl/blob/hcl2/hclsyntax/spec.md'
+  storage "file" {
+    path = "${run_dir_win}/storage"
+  }
 
-listener "tcp" {
-  address = "${vault_server}:${vault_port}"
-  tls_disable = "true"
-}
+  listener "tcp" {
+    address = "${vault_server}:${vault_port}"
+    tls_disable = "true"
+  }
 EOF
+fi
 
 if [ ! -f vault.log ]; then touch vault.log; fi
 
@@ -33,9 +37,12 @@ export VAULT_LOG_LEVEL=debug
 vault server -config="${run_dir}/vault-config.hcl" >> vault.log 2>&1 &
 
 export VAULT_ADDR="http://${vault_server}:${vault_port}" # useless
-cat vault.log
+tail vault.log -n 20
 
 echo "Execute 'export VAULT_ADDR=\"http://${vault_server}:${vault_port}\"' to access Vault server from local without TLS."
 
 # if necessary, init vault
-# vault operator init -key-shares=1 -key-threshold=1
+# vault operator init vau-key-shares=1 -key-threshold=1
+
+# @IMPORTANT current unseal key : akx68yeE9BKKrCNxtgflvyntDXHYBXjjMONEWi4vxjQ=
+#    vault operator unseal akx68yeE9BKKrCNxtgflvyntDXHYBXjjMONEWi4vxjQ=
