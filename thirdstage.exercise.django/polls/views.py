3839272479c97074d2dd36494e8f6dfd2bf5ff9c
Sangmoon Oh <halfface@chollian.net>
2020-12-29 17:55:18 +0900

    ...

--- thirdstage.exercise.django/polls/views.py
+++ thirdstage.exercise.django/polls/views.py
@@ -4,3 +4,17 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hello, World!")
+
+
+def detail(request, question_id):
+  return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+  response = "You are looking at the result of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You are voting on question %s." % question_id)
+
+
