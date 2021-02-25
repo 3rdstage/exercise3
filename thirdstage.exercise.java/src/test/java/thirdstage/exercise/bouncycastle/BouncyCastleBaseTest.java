5c7f1358ad2bb370d05066adb744813b0b91ee5d
Sangmoon Oh <halfface@chollian.net>
2021-02-25 20:23:38 +0900

    Added a simple test for the signing with ECDSA.

--- thirdstage.exercise.java/src/test/java/thirdstage/exercise/bouncycastle/BouncyCastleBaseTest.java
+++ thirdstage.exercise.java/src/test/java/thirdstage/exercise/bouncycastle/BouncyCastleBaseTest.java
@@ -1,15 +1,20 @@
 package thirdstage.exercise.bouncycastle;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.security.PrivateKey;
 import java.security.Provider;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.security.Signature;
 import java.security.spec.ECGenParameterSpec;
 import java.util.Set;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -71,12 +76,7 @@ public class BouncyCastleBaseTest{
   @Test
   public void testGenerateSecp256k1KeyPairs() throws Exception{
 
-    Security.insertProviderAt(new BouncyCastleProvider(), 1);
-
-    KeyPairGenerator kpg = KeyPairGenerator.getInstance("ECDSA", "BC");
-    kpg.initialize(new ECGenParameterSpec("secp256k1"), random);
-
-    final KeyPair kp = kpg.generateKeyPair();
+    final KeyPair kp = this.generateSecp256k1KeyPair();
 
     final Key sk = kp.getPrivate();
     final Key pk = kp.getPublic();
@@ -89,6 +89,60 @@ public class BouncyCastleBaseTest{
     // https://github.com/web3j/web3j/blob/master/utils/src/main/java/org/web3j/utils/Numeric.java#L235
     // @TODO
   }
+
+  private KeyPair generateSecp256k1KeyPair() throws Exception{
+
+    Security.insertProviderAt(new BouncyCastleProvider(), 1);
+
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance("ECDSA", "BC");
+    kpg.initialize(new ECGenParameterSpec("secp256k1"), random);
+
+    return kpg.generateKeyPair();
+  }
+
+
+  /**
+   * @throws Exception
+   *
+   * @see https://docs.oracle.com/javase/9/security/java-cryptography-architecture-jca-reference-guide.htm#JSSEC-GUID-9CF09CE2-9443-4F4E-8095-5CBFC7B697CF
+   */
+  @Test
+  public void testSigningWithSecp256k1KeyPairs() throws Exception{
+
+    final KeyPair kp = this.generateSecp256k1KeyPair();
+
+    final PrivateKey sk = kp.getPrivate();
+    final PublicKey pk = kp.getPublic();
+
+    final Signature signer = Signature.getInstance("SHA256withECDSA");
+    signer.initSign(sk);
+
+    final String msg1 = "We built this city on rock'n roll.";
+    signer.update(msg1.getBytes(StandardCharsets.UTF_8));
+    final byte[] sign1 = signer.sign();
+
+    final String msg2 = "Big city night.";
+    signer.update(msg2.getBytes(StandardCharsets.UTF_8));
+    final byte[] sign2 = signer.sign();
+
+
+    final Signature verifier = Signature.getInstance("SHA256withECDSA");
+    verifier.initVerify(pk);
+
+
+    verifier.update(msg1.getBytes(StandardCharsets.UTF_8));
+    Assertions.assertTrue(verifier.verify(sign1));
+
+    verifier.update(msg2.getBytes(StandardCharsets.UTF_8));
+    Assertions.assertTrue(verifier.verify(sign2));
+
+    verifier.update(msg1.getBytes(StandardCharsets.UTF_8));
+    Assertions.assertFalse(verifier.verify(sign2));
+
+    verifier.update(msg2.getBytes(StandardCharsets.UTF_8));
+    Assertions.assertFalse(verifier.verify(sign1));
+  }
+
 
 
   @Test
