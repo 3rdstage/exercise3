1edddb2dc4aecc6309b332ad1eb22aaed431502d
Sangmoon Oh <halfface@chollian.net>
2021-08-23 20:23:37 +0900

    ...

--- thirdstage.exercise.express/app.js
+++ thirdstage.exercise.express/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
