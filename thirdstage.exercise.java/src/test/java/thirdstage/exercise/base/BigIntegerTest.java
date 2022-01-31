f91b895c2e29d0de8995e10d381a4383e67dbe21
Sangmoon Oh <halfface@chollian.net>
2022-02-01 05:06:23 +0900

    ...

--- thirdstage.exercise.java/src/test/java/thirdstage/exercise/base/BigIntegerTest.java
+++ thirdstage.exercise.java/src/test/java/thirdstage/exercise/base/BigIntegerTest.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Random;
 import org.apache.commons.codec.binary.Hex;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -89,6 +90,35 @@ public class BigIntegerTest{
       this.logger.debug("Number: 0x{}, Byte Array: {}, Char Array: {}",
         num.toString(16), Arrays.toString(bytes), Arrays.toString(chars));
     }
+  }
+
+
+  /**
+   * @throws Exception
+   *
+   * @see https://en.bitcoin.it/wiki/Secp256k1
+   * @see https://github.com/ethereum/eth-keys/blob/v0.3.3/eth_keys/constants.py
+   */
+  @Test
+  public void testToString() throws Exception{
+
+    // secp256k1 field size
+    final String ph = "FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE FFFFFC2F"
+        .replaceAll("\\s", ""); // in hexa decimal
+    final BigInteger p = BigInteger.TWO.pow(256).subtract(BigInteger.TWO.pow(32)).subtract(BigInteger.valueOf(977));
+
+    // secp256k1 order
+    final String nh = "FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE BAAEDCE6 AF48A03B BFD25E8C D0364141"
+        .replaceAll("\\s", ""); //in hexadecimal
+    final BigInteger n = new BigInteger(nh, 16);
+    final String nd = "115792089237316195423570985008687907852837564279074904382605163141518161494337";
+
+    Assertions.assertEquals(ph, p.toString(16).toUpperCase());
+    Assertions.assertEquals(nd, n.toString());
+
+    this.logger.debug("Field Size (2^256 - 2^32 - 977): {}", p.toString());
+    this.logger.debug("Order ({}): {}", nh, n.toString());
+
   }
 
 
