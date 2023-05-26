8ed2ce194b740bcc3b2a43aa269780856eb9182c
Sangmoon Oh <halfface@chollian.net>
2023-05-27 06:21:21 +0900

    ...

--- thirdstage.exercise.ethers6/src/regular-erc20.js
+++ thirdstage.exercise.ethers6/src/regular-erc20.js
@@ -13,6 +13,7 @@ console.log(`Message Prefix : '${ethers.MessagePrefix}'`);
 (async() => {
   const provider = new ethers.JsonRpcProvider('http://127.0.0.1:8545');
   const wallet = ethers.Wallet.fromPhrase(process.env.BIP39_MNEMONIC, provider);
+  console.log(`Generated Account : ${wallet.address}`);
 
   const meta = JSON.parse(fs.readFileSync('contracts/RegularERC20.json'));
   const abi = JSON.stringify(meta.abi);
@@ -22,10 +23,18 @@ console.log(`Message Prefix : '${ethers.MessagePrefix}'`);
   //console.log(typeof(bytecode));
   //console.log(abi);
 
-  const options = {gasLimit : 100000, gasPrice: 0};
+  const options = {gasLimit : 100000, gasPrice: 0, from: wallet.address};
   const factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const contract = await factory.deploy('Rainbow Tokens', 'RGB');
 
-  contract.getAddress().then(addr => console.log(`Contract Deployed - address: ${addr}`));
+
+  console.log(`Contract Deployed`)
+  console.log(Object.keys(contract));
+  const props = [];
+  props.push(["Address", await contract.getAddress()]);
+  props.push(["Symbol", await contract.symbol()]);
+  props.push(["Admin", await contract.admin()]);
+
+  console.table(props);
 
 })();
